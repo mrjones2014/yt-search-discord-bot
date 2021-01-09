07fce260047a46fa05ba620ce9d1c03da56fa4cd
Mat Jones <mjones44@ycp.edu>
2021-01-09 16:30:58 -0500

    remove ts ?. operator causing invalid compilation

--- src/utils/get-first-youtube-result.ts
+++ src/utils/get-first-youtube-result.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiQueryTemplate = (query: string) => {
   const sanitizedQuery = encodeURIComponent(query.replace(/\s/, "+"));
-  return `https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=1&key=${process.env.YOUTUBE_API_KEY}&q=${sanitizedQuery}`;
+  return `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&&type=video&key=${process.env.YOUTUBE_API_KEY}&q=${sanitizedQuery}`;
 }
 
 const watchLinkTemplate = (videoId: string) => `https://www.youtube.com/watch?v=${videoId}`;
@@ -10,18 +10,18 @@ const watchLinkTemplate = (videoId: string) => `https://www.youtube.com/watch?v=
 export const getFirstYoutubeResult = async (query: string) => {
   const result = await axios.get(apiQueryTemplate(query));
   if (result.status !== 200) {
-    throw result.status;
-  }
-
-  const items = result.data?.items;
-  if (items == null || !Array.isArray(items) || items.length < 1) {
     return undefined;
   }
 
-  const firstItem = items[0];
-  if (firstItem == null) {
+  const firstItemId = result.data &&
+                    result.data.items &&
+                    result.data.items[0] &&
+                    result.data.items[0].id &&
+                    result.data.items[0].id.videoId;
+
+  if (firstItemId == null) {
     return undefined;
   }
 
-  return watchLinkTemplate(firstItem.id.videoId);
+  return watchLinkTemplate(firstItemId);
 };
