6dfaa4da87967558f7f7d8f83be4d62e15dd88bb
Mat Jones <mjones44@ycp.edu>
2021-01-09 12:57:07 -0500

    add signature verification

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { pingPong } from "./ping-pong";
+import { verifySignature } from "./verify-signature";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  verifySignature(event);
+
   const pingPongResponse = pingPong(JSON.parse(event.body));
   if (pingPongResponse != null) {
     return {
--- src/verify-signature.ts
+++ src/verify-signature.ts
@@ -1,7 +1,30 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import * as NACL from "tweetnacl";
 
-const verifySignature = (event: APIGatewayProxyEvent) => {
-  const signature = event.headers["x-signature-ed25519"];
-  const timestamp = event.headers["x-signature-timestamp"];
-  const message = sign
+enum Headers {
+  Signature = "X-Signature-Ed25519",
+  Timestamp = "X-Signature-Timestamp",
+}
+
+const cantVerifyResult = () => {
+  throw new Error("Cannot verify signature.");
+};
+
+export const verifySignature = (event: APIGatewayProxyEvent) => {
+  if (event.body == null) {
+    cantVerifyResult();
+  }
+
+  const signature = event.headers[Headers.Signature];
+  const timestamp = event.headers[Headers.Timestamp];
+
+  const isVerified = NACL.sign.detached.verify(
+    Buffer.from(timestamp + event.body),
+    Buffer.from(signature, "hex"),
+    Buffer.from(process.env.DISCORD_API_KEY, "hex"),
+  );
+
+  if (!isVerified) {
+    cantVerifyResult();
+  }
 };
